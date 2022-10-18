63c3293f1e3fc6c8fd9988821dc50244876fbe48
vladian95 <kvankvan1995@gmail.com>
2022-10-18 13:38:53 +0200

    finished testimonails and start contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,19 @@
 import './contact.css';
 
 export default function Contact() {
-  return <div className="Contact" id="Contact"></div>;
+  return (
+    <div className="Contact" id="Contact">
+      <div className="Left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="Right">
+        <h2>Contact.</h2>
+        <form>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/components/testimonails/Testimonails.jsx
+++ src/components/testimonails/Testimonails.jsx
@@ -1,5 +1,53 @@
 import './testimonails.css';
 
 export default function Testimonails() {
-  return <div className="Testimonails" id="Testimonails"></div>;
+  const data = [
+    {
+      id: 1,
+      name: 'Tom Durden',
+      title: 'Senior Developer',
+      img: 'https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+      icon: 'assets/twitter.png',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.',
+    },
+    {
+      id: 2,
+      name: 'Alex Kalinski',
+      title: 'Co-Founder of DELKA',
+      img: 'https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+      icon: 'assets/youtube.png',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ',
+      featured: true,
+    },
+    {
+      id: 3,
+      name: 'Martin Harold',
+      title: 'CEO of ALBI',
+      img: 'https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+      icon: 'assets/linkedin.png',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem',
+    },
+  ];
+
+  return (
+    <div className="Testimonails" id="Testimonails">
+      <h1>Testimonails</h1>
+      <div className="Container">
+        {data.map((d) => (
+          <div className={d.featured ? 'Card featured' : 'Card'}>
+            <div className="Top">
+              <img src="assets/right-arrow.png" className="Left" alt="" />
+              <img src={d.img} className="User" alt="" />
+              <img src={d.icon} className="Right" alt="" />
+            </div>
+            <div className="Center">{d.desc}</div>
+            <div className="Bottom">
+              <h3>{d.name}</h3>
+              <h4>{d.title}</h4>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
