a2ff76f564e1555dcb4853656bba6205d42d9551
vladian95 <kvankvan1995@gmail.com>
2022-10-18 20:43:45 +0200

    finish

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,18 +1,6 @@
 import './intro.css';
-import { init } from 'ityped';
-import { useEffect, useRef } from 'react';
 
 export default function Intro() {
-  const textRef = useRef();
-
-  useEffect(() => {
-    init(textRef.current, {
-      showCursor: false,
-      backDelay: 1500,
-      strings: ['Developer', 'Designer', 'Content Creator'],
-    });
-  }, []);
-
   return (
     <div className="Intro" id="Intro">
       <div className="Left">
@@ -25,7 +13,7 @@ export default function Intro() {
           <h2>Hi There, I'm</h2>
           <h1>Vladyslav Kvanin</h1>
           <h3>
-            Freelance <span ref={textRef}></span>
+            Front-end <span>Developer</span>
           </h3>
         </div>
         <a href="#Portfolio">
